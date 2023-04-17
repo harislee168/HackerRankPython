a28bc5bf81a5ba54a483bb47b5422f9be02ff4d2
harislee168 <haris.lee168@gmail.com>
2023-04-17 20:22:30 +0800

    Update List Comprehension code

--- Basic Data Type/ListComprehensions.py
+++ Basic Data Type/ListComprehensions.py
@@ -5,10 +5,6 @@ if __name__ == '__main__':
     n = int(input())
     
     
-myList = list()
-for myx in range(x+1):
-    for myy in range (y+1):
-        for myz in range (z+1):
-            if myx+myy+myz != n:
-                myList.append([myx,myy,myz])
-print(myList)
+new_list = [[myx, myy, myz] for myx in range (x+1) for myy in range (y+1) for myz in range (z+1) if myx+myy+myz != n]
+
+print(new_list)
